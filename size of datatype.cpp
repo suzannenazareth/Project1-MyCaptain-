0cc07fccfb55d460972879e4821269e937ab278e
suzannenazareth <78813600+suzannenazareth@users.noreply.github.com>
2021-02-10 11:34:44 +0530

    Add files via upload

---
+++ size of datatype.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+ int main()
+{   
+      cout<<"the size of int : "<<sizeof(int)<<" Byte\n";
+      cout<<" the size of float: "<<sizeof(float)<<" Byte\n";
+      cout<<"the size of Char : "<<sizeof(char)<<" Byte\n";
+      cout<< "the size of double:"<<sizeof(double) <<" Byte\n";
+      
+   
+
+
+
+
+
+
+
+}
