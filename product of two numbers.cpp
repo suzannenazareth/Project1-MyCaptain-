72cf9d8962674ffa87ce50fd40317252f009f1c1
suzannenazareth <78813600+suzannenazareth@users.noreply.github.com>
2021-02-10 11:18:45 +0530

    Add files via upload

---
+++ product of two numbers.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+	int main()
+ {
+ 	double x ,y ;
+ 	cout<<"enter your first number";
+ 	cin>>x;
+ 	cout<<"enter your second number";
+ 	cin>>y;
+ 	cout<<"Product is";
+ 	cout<<x*y;
+ 	
+ 	
+ 	
+ 	
+  } 
+	
+	
+	
+
+
+
+
