42d01dc3093f9274bdae5e6394c8049f63c96953
suzannenazareth <78813600+suzannenazareth@users.noreply.github.com>
2021-02-13 14:54:19 +0530

    Add files via upload

---
+++ voting eligibility.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+using namespace std;
+ int main()  
+  { int x;
+  	cout<<"please enter your age:";
+  	cin>>x;
+  	if(x<18)
+  	{  int x;
+  		cout<<"you are not eligible";
+  	   
+
+	  } 
+	  
+	  else
+	  {
+	  	cout<<"your are eligible";
+	  	
+	  }
+	  
+  	
+  	
+  	
+  	
+	}  
+
+
+
