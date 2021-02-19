04c79bb1037816e9494002c001c198efda317cef
suzannenazareth <78813600+suzannenazareth@users.noreply.github.com>
2021-02-19 21:45:37 +0530

    Add files via upload

---
+++ arrays.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{   cout<<"ENTER ELEMENTS:" ;   
+    int elements[]={1 ,2, 3, 5, 4};
+	cout<<elements[0]<<endl;
+    cout<<elements[1]<<endl;
+    cout<<elements[2]<<endl;
+    cout<<elements[3]<<endl;
+    cout<<elements[4]<<endl;
+    
+    cout<<"YOU ENTERED:";
+    int numbers []={1 ,2, 3, 5, 4};
+	cout<<numbers[0]<<endl;
+    cout<<numbers[1]<<endl;
+    cout<<numbers[2]<<endl;
+    cout<<numbers[3]<<endl;
+     cout<<numbers[4]<<endl;
+    
+
+
+}
